b439dc8a855d2319be24d1e59f896abc8bee6f7d
TheoCaesar <theocaesar7@gmail.com>
2024-01-11 08:04:58 +0000

    jQuerySlider.js

--- jQuerySlider.js
+++ jQuerySlider.js
@@ -17,20 +17,42 @@ $(window).on("load", ()=>{
     $("#next").on("click",()=>{
         counter++;
         if (counter == numOfImgs) {
+            $("#slider ul").clone().appendTo("#slider");
+            $("#slider ul").last().css("left", `${imgWidth}px `)
+
+            leftPosition = `${totalWidth}`;
+
+            $("#slider ul").last().animate({left:0}, 700)
+            $("#slider ul").first().animate({left:leftPosition}, 700, ()=> {
+                $("#slider ul").first().remove();
+            })
+
             counter = 0;
+        } else {
+            leftPosition = `-${counter*imgWidth}px`;
+            
+            $("#slider ul").animate({"left":leftPosition}, 500);
         }
-        leftPosition = `-${counter*imgWidth}px`;
-        
-        $("#slider ul").animate({"left":leftPosition}, (counter == 0) ? 0 : 500);
     })
 
     $("#prev").on("click",()=>{
         counter--;
         if (counter < 0) {
             counter = numOfImgs-1;
+
+            $("#slider ul").clone().appendTo("#slider");
+            $("#slider ul").last().css("left", `-${totalWidth}`)
+
+            leftPosition = `-${counter*imgWidth}`;
+
+            $("#slider ul").last().animate({left:leftPosition}, 700);
+            $("#slider ul").first().animate({left:imgWidth+"px"}, 700, ()=> {
+                $("#slider ul").first().remove();
+            })
+        } else {
+            leftPosition = `-${counter*imgWidth}px`;
+            
+            $("#slider ul").animate({"left":leftPosition},  500);
         }
-        leftPosition = `-${counter*imgWidth}px`;
-        
-        $("#slider ul").animate({"left":leftPosition}, (counter == numOfImgs - 1) ? 0 : 2500);
     })
 });
