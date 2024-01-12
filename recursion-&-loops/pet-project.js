2409a9182b60cfcdba28c1f608a49440ce0d9c1f
TheoCaesar <theocaesar7@gmail.com>
2024-01-12 10:11:21 +0000

    pet-project.js

--- recursion-&-loops/pet-project.js
+++ recursion-&-loops/pet-project.js
@@ -1,9 +1,7 @@
 let counter =  1;
 let numOfDivs = $('#container div').length;
 function contentRotator(){
-    console.log(counter, numOfDivs);
     $(`#container div:nth-child(${counter})`).fadeIn(2000, ()=>{
-    //    if ($(this).is("#container div:last-child")) {
         if (counter == numOfDivs) {
         console.log("this is last child", $(this));
             setTimeout(() => {
